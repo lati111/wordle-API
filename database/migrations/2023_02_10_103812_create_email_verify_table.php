62d7e80a20ec4b766da3226dd9371f2130c8297c
lati111 <jvzelst111@gmail.com>
2023-02-10 11:41:53 +0100

    Added email verification framework

---
+++ database/migrations/2023_02_10_103812_create_email_verify_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('email_verify', function (Blueprint $table) {
+            $table->id();
+            $table->foreignUuid("user_uuid");
+            $table->timestamps();
+
+            $table->foreign('user_uuid')->references('uuid')->on('users')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('email_verify');
+    }
+};
