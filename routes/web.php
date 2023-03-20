2c7203f556a80410b2e4800c4a0db86806312817
lati111 <jvzelst111@gmail.com>
2023-03-20 22:15:53 +0100

    Fixed docker file

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,7 @@
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+
+Route::get('/', function () {
+    return 'Hello World';
+});
