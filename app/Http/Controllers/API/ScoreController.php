0c749e41f7b98bdd4f844275dbc316f33dea3485
LucasDH123 <75067759+LucasDH123@users.noreply.github.com>
2023-03-24 10:24:09 +0100

    fixed readme, so it aligned with the actual code

---
+++ app/Http/Controllers/API/ScoreController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ScoreController extends Controller
+{
+   
+    public function createScore(Request $request){
+            try {
+                Request::validate
+            }
+    }
+}
